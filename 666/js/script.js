6c82045df6e8b87b953ac1bc4778bbe7418f56e9
wangshizhao <903967636@qq.com>
2018-01-16 23:35:14 +0800

    Signed-off-by: wangshizhao <903967636@qq.com>

--- 666/js/script.js
+++ 666/js/script.js
@@ -27,3 +27,66 @@ $(function() {
 		});
 	};
 });
+
+window.onload=function(){
+	var d=document.getElementById('ig1');
+	d.onmouseover=function(){
+		this.src='img/zyy.png';
+	};
+	d.onmouseout=function(){
+		this.src='img/zy.png';
+	};
+	var e=document.getElementById('ig2');
+	e.onmouseover=function(){
+		this.src='img/sll.png';
+	};
+	e.onmouseout=function(){
+		this.src='img/sl.png';
+	};
+	var f=document.getElementById('ig3');
+	f.onmouseover=function(){
+		this.src='img/zff.png';
+	};
+	f.onmouseout=function(){
+		this.src='img/zf.png';
+	};
+	var g=document.getElementById('ig4');
+	g.onmouseover=function(){
+		this.src='img/cwjj.png';
+	};
+	g.onmouseout=function(){
+		this.src='img/cwj.png';
+	};
+	var h=document.getElementById('ig5');
+	h.onmouseover=function(){
+		this.src='img/hyy.png';
+	};
+	h.onmouseout=function(){
+		this.src='img/hy.png';
+	};
+	var i=document.getElementById('ig6');
+	i.onmouseover=function(){
+		this.src='img/dcc.png';
+	};
+	i.onmouseout=function(){
+		this.src='img/dc.png';
+	};
+	var j=document.getElementById('ig7');
+	j.onmouseover=function(){
+		this.src='img/lbb.png';
+	};
+	j.onmouseout=function(){
+		this.src='img/lb.png';
+	};
+	var k=document.getElementById('ig8');
+	k.onmouseover=function(){
+		this.src='img/gyy.png';
+	};
+	k.onmouseout=function(){
+		this.src='img/gy.png';
+	};
+};
+
+
+
+
